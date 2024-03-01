0c31d9c6a8e7929a0c3708b260c260ccb8d47fc6
aakash mehta <aakashmehta3105@.com>
2024-03-01 10:13:09 +0530

    today update

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
-console.log("hello");
+const express = require('express')
+require('dotenv').config()
+const app = express()
+
+
+console.log(process.env.port)
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(process.env.port,()=>{
+    console.log("listening..",process.env.port);
+})
